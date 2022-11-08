0d12030103d9ab76cc21f2294b86b70e08d83000
Ajand Mardalizad <aseedgeek@gmail.com>
2022-11-08 19:54:02 +0300

    initial admin creation added

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ const {
 const main = async () => {
   const provider = new ethers.providers.JsonRpcProvider(config.RPC_URL);
 
+  Admin.methods.queries
+    .getAll()
+    .then((admins) => {
+      if (admins.length === 0) {
+        Admin.methods.commands
+          .create(config.INITIAL_ADMIN, true)
+          .then((res) => console.log("super admin created"))
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+
   apiServer(provider);
 };
 
